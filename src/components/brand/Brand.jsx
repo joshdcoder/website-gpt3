ccccf2a91b70e21acaee22d7a181f66d52be750f
JosueGS <joshdiscover97@gmail.com>
2022-01-21 16:04:35 -0500

    header, navbar, brand terminados

---
+++ src/components/brand/Brand.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import './brand.css';
+import { google, slack, atlassian, dropbox, shopify} from './imports';
+
+const Brand = () => {
+    return (
+        <div className="gpt3__brand section__padding">
+            <div>
+                <img src={google} alt='' />
+            </div>
+            <div>
+                <img src={slack} alt='' />
+            </div>
+            <div>
+                <img src={atlassian} alt='' />
+            </div>
+            <div>
+                <img src={dropbox} alt='' />
+            </div>
+            <div>
+                <img src={shopify} alt='' />
+            </div>
+        </div>
+    )
+}
+
+export default Brand;
---
+++ src/containers/features/Features.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './features.css';
+
+const Features = () => {
+    return (
+        <div>
+            Featuares
+        </div>
+    )
+}
+
+export default Features
